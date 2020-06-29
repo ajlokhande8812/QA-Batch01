c013bc74f88678da66c30ee166d1b330096e66be
ajinkya <aju.lokhande8812@gmail.com>
2020-06-30 01:18:58 +0530

    added maven project

---
+++ batch01/src/main/java/qaseleniumjava/batch01/App.java
@@ -0,0 +1,13 @@
+package qaseleniumjava.batch01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
